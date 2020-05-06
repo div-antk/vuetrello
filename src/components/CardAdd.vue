4e402113faa319f95d0a6ba9d13fc045771d4401
div-antk <51283468+owaii@users.noreply.github.com>
2020-05-06 14:24:10 +0900

    add card_add_form

---
+++ src/components/CardAdd.vue
@@ -0,0 +1,35 @@
+<template>
+  <form class="addcard" @submit.prevent="addCardList">
+    <input v-model="body"
+    type="text"
+    class="text-input"
+    placeholder="Add new card"
+  />
+    <button type="submit" class="add-button">
+      Add
+    </button>
+  </form>
+</template>
+
+<script>
+  export default {
+    props: {
+      listIndex: {
+        type: Number,
+        required: true,
+      }
+    },
+    data: function() {
+      return {
+        body: '',
+        }
+      },
+  methods: {
+    addCardToList: function() {
+      this.$store.dispatch('addCardTolist', { body: this.body, listIndex: this.listIndex })
+      this.body = ''
+    }
+  }
+}
+
+</script>
