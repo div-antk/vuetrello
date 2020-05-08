2305b2dec20980347b08c2d51234cc58dbbc03e1
div-antk <51283468+owaii@users.noreply.github.com>
2020-05-08 19:21:49 +0900

    done save_moving_card

--- src/components/List.vue
+++ src/components/List.vue
@@ -5,10 +5,9 @@
       <p class="list-counter">total: {{ totalCardInList }}</p>
       <div class="deletelist" @click="removeList">×</div>
     </div>
-    <draggable group="cards">
-    <!-- <draggable group="cards"
+    <draggable group="cards"
                 :list="cards"
-                @end="$emit('change')"> -->
+                @end="$emit('change')">
       <card v-for="(item, index) in cards"
             :body="item.body"
             :key="item.id"
