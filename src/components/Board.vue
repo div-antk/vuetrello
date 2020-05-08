4557c3cbb830fcb69891ab9987d37476ede4c6b3
div-antk <51283468+owaii@users.noreply.github.com>
2020-05-08 19:34:04 +0900

    moving_list

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -5,7 +5,9 @@
     </header>
     <main>
       <p class="info-line">All: {{ totalCardCount }} tasks</p>
-      <div class="list-index">
+      <draggable :list="lists"
+                  @end="movingList"
+                  class="list-index">
           <!-- v-bindは:と省略して記述できる。 -->
           <list v-for="(item, index) in lists"
                 :key="item.id"
@@ -15,17 +17,19 @@
                 @change="movingCard"
           />
         <list-add />
-      </div>
+      </draggable>
     </main>
   </div>
 </template>
 
 <script>
+import draggable from 'vuedraggable'
 import ListAdd from './ListAdd'
 import List from './List'
 import { mapState } from 'vuex'
 export default {
   components: {
+    draggable,
     ListAdd,
     List,
   },
@@ -40,6 +44,9 @@ export default {
   },
   methods: {
     movingCard: function() {
+      this.$store.dispatch('updateList', { lists: this.lists })
+    },
+    movingList: function() {
       this.$store.dispatch('updateList', { lists: this.lists })
     },
   }
